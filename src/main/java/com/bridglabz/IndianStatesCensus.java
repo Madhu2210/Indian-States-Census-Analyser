f3d5d2688778618aa73d0b12d9051df335b52f97
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-05-31 23:07:22 +0530

    Added: Welcome to Indian States Census Program.

---
+++ src/main/java/com/bridglabz/IndianStatesCensus.java
@@ -0,0 +1,7 @@
+package com.bridglabz;
+
+public class IndianStatesCensus {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Indian States Census Program.");
+    }
+}
